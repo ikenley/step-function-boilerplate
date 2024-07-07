f07dad0b6e779f22e0c7414b68c26a15269d7b65
ikenley <ikenley6@gmail.com>
2024-07-07 15:49:17 -0400

    sfn

--- iac/modules/main/outputs.tf
+++ iac/modules/main/outputs.tf
@@ -1,12 +1,12 @@
 # ------------------------------------------------------------------------------
-# revisit_prediction.tf
+# sfn_state_machine.tf
 # ------------------------------------------------------------------------------
 
-# resource "aws_ssm_parameter" "revisit_prediction__function_arn" {
-#   name  = "${local.output_prefix}/revisit_prediction/function_arn"
-#   type  = "SecureString"
-#   value = module.revisit_prediction_lambda.lambda_function_arn
-# }
+resource "aws_ssm_parameter" "sfn_state_machine_arn" {
+  name  = "${local.output_prefix}/sfn_state_machine_arn"
+  type  = "String"
+  value = aws_sfn_state_machine.sfn.arn
+}
 
 # resource "aws_ssm_parameter" "revisit_prediction__function_name" {
 #   name  = "${local.output_prefix}/revisit_prediction/function_name"
--- iac/modules/main/sfn_state_machine.tf
+++ iac/modules/main/sfn_state_machine.tf
@@ -6,18 +6,36 @@ locals {
   state_machine_id = "${local.id}-sfn"
 }
 
-resource "aws_sfn_state_machine" "sfn_state_machine" {
+resource "aws_sfn_state_machine" "sfn" {
   name     = local.state_machine_id
-  role_arn = aws_iam_role.iam_for_sfn.arn
+  role_arn = aws_iam_role.sfn.arn
 
   definition = <<EOF
 {
-  "Comment": "A Hello World example of the Amazon States Language using an AWS Lambda Function",
-  "StartAt": "HelloWorld",
+  "Comment": "A description of my state machine",
+  "StartAt": "hello_world_lambda",
   "States": {
-    "HelloWorld": {
+    "hello_world_lambda": {
       "Type": "Task",
-      "Resource": "${aws_lambda_function.lambda.arn}",
+      "Resource": "arn:aws:states:::lambda:invoke",
+      "OutputPath": "$.Payload",
+      "Parameters": {
+        "Payload.$": "$",
+        "FunctionName": "${module.hello_world_lambda.lambda_function_arn}:$LATEST"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException",
+            "Lambda.TooManyRequestsException"
+          ],
+          "IntervalSeconds": 1,
+          "MaxAttempts": 3,
+          "BackoffRate": 2
+        }
+      ],
       "End": true
     }
   }
@@ -32,7 +50,87 @@ EOF
 }
 
 resource "aws_cloudwatch_log_group" "sfn" {
-  name = state_machine_id
+  name = local.state_machine_id
 
   tags = local.tags
+}
+
+resource "aws_iam_role" "sfn" {
+  name = local.state_machine_id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "sfn" {
+  role       = aws_iam_role.sfn.name
+  policy_arn = aws_iam_policy.sfn.arn
+}
+
+resource "aws_iam_policy" "sfn" {
+  name        = local.state_machine_id
+  path        = "/"
+  description = "Main policy for ${local.id}"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "Cloudwatch",
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogDelivery",
+          "logs:GetLogDelivery",
+          "logs:UpdateLogDelivery",
+          "logs:DeleteLogDelivery",
+          "logs:ListLogDeliveries",
+          "logs:PutResourcePolicy",
+          "logs:DescribeResourcePolicies",
+          "logs:DescribeLogGroups"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "xray",
+        "Effect" : "Allow",
+        "Action" : [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords",
+          "xray:GetSamplingRules",
+          "xray:GetSamplingTargets"
+        ],
+        "Resource" : [
+          "*"
+        ]
+      },
+      {
+        "Sid" : "InvokeLambda",
+        "Effect" : "Allow",
+        "Action" : [
+          "lambda:InvokeFunction"
+        ],
+        "Resource" : [
+          "${module.hello_world_lambda.lambda_function_arn}",
+          "${module.hello_world_lambda.lambda_function_arn}:*"
+        ]
+      }
+    ]
+  })
 }
