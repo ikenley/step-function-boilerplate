410223375b9caac375bea1c34c10254e30a3ea31
ikenley <ikenley6@gmail.com>
2024-07-17 20:16:14 -0400

    lambda_function

--- iac/modules/main/lambda/ai-image/src/lambda_function.mjs
+++ iac/modules/main/lambda/ai-image/src/lambda_function.mjs
@@ -1,7 +1,21 @@
-console.log("Loading function");
+import { randomUUID } from "crypto";
 
-export const handler = (event, context, callback) => {
+export const handler = async (event, context, callback) => {
   console.log(`event= ${JSON.stringify(event)}`);
+
+  let result = {};
+  if (event.command === "GetMetadata") {
+    result = getMetadata();
+  }
+
+  console.log(`result= ${JSON.stringify(result)}`);
+
+  callback(null, result);
+};
+
+/** Creates imageId and other metadata */
+export const getMetadata = () => {
+  return { imageId: randomUUID() };
 };
 
 export default handler;
